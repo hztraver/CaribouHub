9d19a51d019954450b02bf4e25a7a4d4a8b1e84a
caethier <ca.ethier2@gmail.com>
2026-02-22 13:41:23 -0800

    adapted Hana's script to perform change detection between 1985-2020 for lichen and deciduous shrubs

--- scripts/claire/02_pft_change_FMCH_range.R
+++ scripts/claire/02_pft_change_FMCH_range.R
@@ -1,91 +1,68 @@
 library(terra)
-library(dplyr)
 library(data.table)
-library(magrittr)
+library(ggplot2)
 
-#### SUMMARY STATISTICS FOR TOTAL PERCENT LICHEN COVER
-#### Calculate overall percent cover in total,  summer and winter range 
+# --- 1. SETTINGS & PATHS ---
+root_path  <- "C:/Users/caeth/Documents/Data/ABOVE PFT 2005-2020/AK_Yukon_PFT_TopCover_2032_1.1-20260222_205533"
+output_dir <- file.path(root_path, "Clipped_PFT_Data")
+setwd(root_path)
 
-# Folder of lichen percent cover 30 m layers
-dir = "E:/Caribou/lichen_clipped/"
-lichen.layers = list.files(path = "E:/Caribou/lichen_clipped", pattern = "*.tif$")
+# --- 2. LOAD AND AGGREGATE ---
+# Loading the multi-layer files we created
+r1985 <- rast(file.path(output_dir, "PFT_masked_1985.tif"))
+r2020 <- rast(file.path(output_dir, "PFT_masked_2020.tif"))
 
-## Aggregate 30 m percent cover to 900 m spatial resolution to reduce file size
-## since we can just sum percent cover in larger areas without losing information 
-# it will be faster to plot and summarize the data at a coarser resolution 
-for (layer in lichen.layers){
-  
-  print(layer)
-  
-  # folder for 900 m layers
-  layer.out = gsub(".tif", "_900m.tif", layer)
-  path.out = paste0("E:/Caribou/lichen_clipped/aggregate_1km/", layer.out)
-  
-  # aggregate pixels by a factor of 30 and sum percent cover values 
-  # then divide by the number of pixels to get percent cover within each 900 m cell
-  # (alternatively just use fun = "mean", but I thought this made intuitive sense)
-  r = rast(paste0(dir, layer)) %>% terra::aggregate(., fact = 30, fun = "sum")
-  r = r/900
-  
-  writeRaster(r, filename = path.out, overwrite = T)
-  gc(verbose = F)
-}
+# Ensure we are looking at the same layers
+# Note: 2020 has many layers, 1985 only has DeciduousShrub and tmLichenLight
+common_layers <- c("DeciduousShrub", "tmLichenLight")
+s1985 <- r1985[[common_layers]]
+s2020 <- r2020[[common_layers]]
 
-## Sum the 900 m layers to get overall percent cover in total, summer and winter range
-## for each time period
-dir.900m = "E:/Caribou/lichen_clipped/aggregate_1km/"
-list.900m = list.files(path = "E:/Caribou/lichen_clipped/aggregate_1km/", pattern = "*.tif$")
-time.periods = c("1985", "1990", "1995", "2000", "2005", "2010", "2015", "2020")
+# Aggregate to 900m (fact=30) as per your method
+message("Aggregating to 900m...")
+s1985_900m <- terra::aggregate(s1985, fact = 30, fun = "mean")
+s2020_900m <- terra::aggregate(s2020, fact = 30, fun = "mean")
 
-## Combine range shapefiles into one 
-summer = vect("E:/Caribou/lichen_clipped/projected_range/summer_range_macander.shp")
-winter = vect("E:/Caribou/lichen_clipped/projected_range/winter_range_macander.shp")
-total = vect("E:/Caribou/lichen_clipped/projected_range/total_range_macander.shp")
+# --- 3. CALCULATE SPATIAL DIFFERENCE ---
+# Change = 2020 - 1985
+pft_diff <- s2020_900m - s1985_900m
+names(pft_diff) <- paste0("Diff_", names(s1985_900m))
 
-range = rbind(total, summer, winter)
-range$RANGE = c("Total", "Summer", "Winter")
+# --- 4. CALCULATE ZONAL STATISTICS (Single Range) ---
 
-# Calculate total area of ranges (tota, summer, winter) in km2
-range.area = data.frame(Range = range$RANGE, Area_km2 = expanse(range, unit = "km"))
+# Ensure your single range map is loaded and projected
+# FMCH_vect should be the SpatVector you created in the previous step
+FMCH_range_proj <- project(FMCH_vect, crs(pft_diff))
 
-# Write the total percent cover to a data frame
-dt = range.area
+# Get mean change for the entire range
+# zonal() returns a data.frame with the mean of each layer in pft_diff
+diff_summary <- terra::zonal(pft_diff, FMCH_range_proj, fun = "mean", na.rm = TRUE)
 
-# Note that an alternative method to get means across different ranges is use terra:zonal(fun = "mean") 
-# with the raster & range boundary polygon, instead of writing the raster values to a data.table.
-# but the zonal method is slower because calculating a mean on a raster is much slower than operations on a data.table
+# Add a column to identify the area
+diff_summary$Range <- "Fortymile_Expanded_Range"
 
-for (time in time.periods) {
-  
-  print(time)
-  
-  # Percent cover layers for summer range, total range, winter range (in that order)
-  layers = grep(time, list.900m, value = T)
-  
-  # get the raster of percent cover for total range
-  # extract values as a data.table and calculate a mean
-  total = rast(paste0(dir.900m, layers[2])) %>% values() %>% na.omit() %>% as.data.table()
-  percent.total = mean(total$cover)
-  
-  # summer range mean percent cover 
-  summer = rast(paste0(dir.900m, layers[1])) %>% values() %>% na.omit() %>% as.data.table()
-  percent.summer = mean(summer$cover)
-  
-  # winter range mean percent cover
-  winter = rast(paste0(dir.900m, layers[3])) %>% values() %>% na.omit() %>% as.data.table()
-  percent.winter = mean(winter$cover)
-  
-  # stack mean cover values and append to data table 
-  percent.cover = c(percent.total, percent.summer, percent.winter)
-  out = data.frame(percent.cover)
-  out = set_names(out, paste0("percent_cover_", time))
-  dt = cbind(dt, out)
-  
-  # clear memory 
-  gc(verbose = F)
-}
+print("Mean Percent Cover Change (1985 to 2020):")
+print(diff_summary)
 
-dt
-write.csv(dt, "E:/Caribou/lichen_clipped/lichen_percent_change.csv")
+# --- 5. SIMPLE PLOT (Balanced Shared Scale) ---
 
-#### Visualize change in 900 m pixels 
+# 1. Determine the global range of values to ensure identical scales
+# We find the max absolute value to keep zero in the center
+max_val <- max(abs(minmax(pft_diff)), na.rm = TRUE)
+shared_range <- c(-max_val, max_val)
+
+# 2. Define a diverging color palette (Red = Decrease, White = No Change, Blue = Increase)
+# You can swap "blue" and "red" depending on which direction you want to emphasize
+diff_cols <- colorRampPalette(c("red", "white", "blue"))(100)
+
+# 3. Plot with the shared scale
+plot(pft_diff, 
+     main = c("Deciduous Shrub Change (1985-2020)", 
+              "Lichen Light Change (1985-2020)"),
+     range = shared_range,  # This forces the same scale limits
+     col = diff_cols,       # This applies the same colors
+     nc = 2)                # Forces plots to be side-by-side (2 columns)
+
+# --- 6. SAVE RESULTS ---
+writeRaster(pft_diff, file.path(output_dir, "PFT_Difference_1985_2020_900m.tif"), overwrite = TRUE)
+write.csv(diff_summary, file.path(output_dir, "FMCH_PFT_Change_Summary_1985_2020.csv"), row.names = FALSE)
